269bbe3efd15d921023cda178b1ffc90fbbb4804
vicky <prajwalkawale01@hotmail.com>
2023-09-02 18:33:00 +0530

    practiced with functions

---
+++ day_04.js
@@ -0,0 +1,22 @@
+let one= "A";
+let two= "B";
+// console.log(one>two); -----------false
+console.log(one<two);
+
+let text = "Hello";
+text+=" World";
+console.log(text);
+
+let dataT = "Boolean" ;
+let dataT2 = "Null";
+
+console.log(dataT===dataT2);
+
+let number = BigInt(1234567891234566666666666);
+console.log(number);
+
+function myFunction(p1 , p2){
+    return p1 * p2 ;
+}
+let result = myFunction(4,5);
+console.log(result);
