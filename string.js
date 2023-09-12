fd8c25a4b658e15c2e6d5fc8f331fdb065465a21
vicky <prajwalkawale01@hotmail.com>
2023-09-12 17:25:18 +0530

    "string methods"

--- string.js
+++ string.js
@@ -14,3 +14,5 @@ console.log(typeof person1 + " "+ typeof person2)
 console.log(person1==person2); //true
 console.log(person1===person2); //False
 console.log(person3==person4);
+
+console.log("17"+"hello javascript") // in double qoted comma the number atomatically javascript assume to it as a string
---
+++ string_method/upperCase.js
@@ -0,0 +1,19 @@
+let $_1 = "hello console";
+console.log("upperCase::",$_1.toUpperCase());
+
+// convert lowerCase 
+
+let $_2 = "HELLO JAVASCRIPT";
+console.log("loweCase::", $_2.toLowerCase());
+
+function myFunction(){
+    let $_3 = "hello";
+    console.log($_3.toUpperCase())
+}
+myFunction();
+
+// CONCAT METHOD 
+
+let $_4 = "Hello";
+let $_5 = "JavaScript";
+console.log($_4.concat(" ",$_5));
